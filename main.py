821d26335cc8ea189eaf52c9ec9b70d88772eee4
londony11 <londony11@gmail.com>
2023-09-28 05:24:29 +0200

    complete game with 2 real player

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 def print_board(board):
-    for row in board:
+    for i, row in enumerate(board):
         print(" | ".join(row))
-        if row < len(board):
-            print("---------")
+        if i < len(board) - 1:
+            print("-" * len(board[0] * 3))
 
 
 def check_winner(board, player):
-    return 0
+    for row in board:
+        if all(cell == player for cell in row):
+            return True
+
+    for col in range(3):
+        if all(board[row][col] == player for row in range(3)):
+            return True
+
+    if all(board[i][i] == player for i in range(3)):
+        return True
+    if all(board[i][2 - i] == player for i in range(3)):
+        return True
+
+    return False
 
 
 def is_board_full(board):
-    return 0
+    return all(cell != " " for row in board for cell in row)
 
 
 def main():
@@ -20,7 +33,36 @@ def main():
 
     print("Welcome to Tic Tac Toe!\n")
 
+    while True:
+        print_board(board)
+
+        row = int(input(f"\nPlayer {current_player}, enter row (0, 1, 2): "))
+        col = int(input(f"Player {current_player}, enter column (0, 1, 2): "))
+        print(" ")
+
+        if board[row][col] == " ":
+            board[row][col] = current_player
+        else:
+            print("The position is not available.\n")
+            continue
+
+        if check_winner(board, current_player):
+            winner = current_player
+            break
+
+        if is_board_full(board):
+            break
+
+        if current_player == "X":
+            current_player = "O"
+        else:
+            current_player = "X"
+
     print_board(board)
+    if winner:
+        print(f"Player {winner} wins!")
+    else:
+        print("It's a tie!")
 
 
 if __name__ == '__main__':
